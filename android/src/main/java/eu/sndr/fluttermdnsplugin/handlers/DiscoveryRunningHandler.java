9b0f0802d5d80571f49577d36974e91693367bb0
RaghuMudem <raghumudem@gmail.com>
2021-06-25 14:23:01 +0200

    Fix for NsdManager: MulticastLock under-locked multicastLock

--- android/src/main/java/eu/sndr/fluttermdnsplugin/handlers/DiscoveryRunningHandler.java
+++ android/src/main/java/eu/sndr/fluttermdnsplugin/handlers/DiscoveryRunningHandler.java
@@ -32,8 +32,12 @@ public class DiscoveryRunningHandler implements EventChannel.StreamHandler {
     }
 
     public void onDiscoveryStopped(){
-        multicastLock.release();
-        handler.post(() -> sink.success(false));
+        if (multicastLock.isHeld()) {
+            //This is the fix for android crash 'NsdManager MulticastLock under-locked multicastLock'.
+            //https://stackoverflow.com/a/14949367/468952
+            multicastLock.release();
+            handler.post(() -> sink.success(false));
+        }
     }
 
     public void onDiscoveryStarted(){
